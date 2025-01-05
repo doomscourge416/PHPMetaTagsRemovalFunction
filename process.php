ae533046ccf26937838ef5adf96b6e7cb25434ac
Pavel_Stadium <jet_7@list.ru>
2025-01-05 11:26:09 +0300

    class iterator interface

--- process.php
+++ process.php
@@ -1,24 +1,73 @@
 <?php
 
-function removeMetaTags($html) {
-    // Создаем новый объект DOMDocument
-    $dom = new DOMDocument();
+class MetaTagIterator implements Iterator {
+    private $dom;
+    private $elements;
+    private $position = 0;
 
-    // Загружаем HTML-код. Используем @ для подавления ошибок.
-    @$dom->loadHTML($html);
+    public function __construct($html) {
+        // Создаем новый объект DOMDocument
+        $this->dom = new DOMDocument();
+        @$this->dom->loadHTML($html);
+        $this->elements = [];
 
-    // Удаляем мета-теги
-    $tagsToRemove = ['title', 'meta[name="description"]', 'meta[name="keywords"]'];
+        // Собираем мета-теги и заголовок
+        // Получаем все заголовки и мета-теги
+        $titleElements = $this->dom->getElementsByTagName('title');
+        foreach ($titleElements as $element) {
+            $this->elements[] = $element;
+        }
 
-    foreach ($tagsToRemove as $tag) {
-        $elements = $dom->getElementsByTagName($tag);
-        while ($elements->length > 0) {
-            $elements->item(0)->parentNode->removeChild($elements->item(0));
+        $metaElements = $this->dom->getElementsByTagName('meta');
+        foreach ($metaElements as $element) {
+            $this->elements[] = $element;
         }
     }
 
-    // Получаем измененный HTML
-    return $dom->saveHTML();
+    public function current() {
+        return $this->elements[$this->position];
+    }
+
+    public function key() {
+        return $this->position;
+    }
+
+    public function next() {
+        ++$this->position;
+    }
+
+    public function rewind() {
+        $this->position = 0;
+    }
+
+    public function valid() {
+        return isset($this->elements[$this->position]);
+    }
+
+    public function removeCurrent() {
+        if ($this->valid()) {
+            $this->current()->parentNode->removeChild($this->current());
+        }
+    }
+
+    public function getDOM() {
+        return $this->dom; // Метод для получения DOM
+    }
 }
 
-?>
+function removeMetaTags($html) {
+    // Создаем итератор
+    $iterator = new MetaTagIterator($html);
+    
+    // Удаляем элементы title и все meta-теги
+    while ($iterator->valid()) {
+        $tag = $iterator->current();
+        if ($tag->getAttribute('name') === 'description' || $tag->getAttribute('name') === 'keywords' || $tag instanceof DOMElement && $tag->nodeName === 'title') {
+            $iterator->removeCurrent();
+        }
+        $iterator->next();
+    }
+
+    // Получаем измененный HTML
+    return $iterator->getDOM()->saveHTML(); // Используем метод для доступа к DOM
+}
